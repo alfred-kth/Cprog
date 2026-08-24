5edb00b07a8c3d0a3c205546782d0e6541552812
Alfred <alfredandersson32@gmail.com>
2026-08-24 20:24:42 +0200

    changed it

---
+++ Facit/ex1/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    int distance = 1200;
+
+    // this is also a comment
+    printf("You are %d miles away.\nNo I'm not\nAre you sure?\n", distance);
+
+
+}
